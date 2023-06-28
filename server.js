2ffe66dbe1bba6ecb70dfb75118b416a5ccc8ffb
ohziiilov3 <klbaskerville0520@gmail.com>
2023-06-28 06:25:18 -0700

    chore: setup express, root route working

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+
+app.get('/',(req,res)=>{
+    res.send('<h1> Express is Working!</h1>');
+});
+
+
+
+
+
+
+
+
+
+app.listen(PORT,()=>{
+    console.log(`Listening on port:${PORT}`);
+});
+
+
+
+
+
+
