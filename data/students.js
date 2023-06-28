f87efc04112bbf05e6a35bce9d569497dbab60c1
ohziiilov3 <klbaskerville0520@gmail.com>
2023-06-28 06:59:08 -0700

    chore: setup home view, and public folder

---
+++ data/students.js
@@ -0,0 +1,30 @@
+const students = [
+  {
+    name: "Fred Fline",
+    role: "Full Stack",
+    employed: true,
+    image:
+      "https://images.unsplash.com/photo-1534665482403-a909d0d97c67?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8c29mdHdhcmUlMjBlbmdpbmVlcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=400&q=60",
+  },
+  {
+    name: "Jalen Jacoby",
+    role: "Frontend",
+    employed: false,
+    image:
+      "https://images.unsplash.com/photo-1549692520-acc6669e2f0c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8c29mdHdhcmUlMjBlbmdpbmVlcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=400&q=60",
+  },
+  {
+    name: "Ashley Flowers",
+    role: "Backend",
+    employed: false,
+    image:
+      "https://images.unsplash.com/photo-1573164713988-8665fc963095?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHNvZnR3YXJlJTIwZW5naW5lZXJ8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=400&q=60",
+  },
+  {
+    name: "Dan Marino",
+    role: "Frontend",
+    employed: true,
+    image:
+      "https://images.unsplash.com/photo-1581092795360-fd1ca04f0952?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHNvZnR3YXJlJTIwZW5naW5lZXJ8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=400&q=60",
+  },
+];
--- server.js
+++ server.js
@@ -3,11 +3,23 @@ const app = express();
 
 const PORT = 3000;
 
+app.set("view engine", "ejs");
+
+// app.set("views", path.join(__dirname, "views"));
+
+//  connect css files and js files 
+app.use(express.static("public"));
+
+
 
 app.get('/',(req,res)=>{
-    res.send('<h1> Express is Working!</h1>');
+    res.redirect('home');
 });
 
+
+app.get("/home", (req, res) => {
+  res.render('home');
+});
 
 
 
